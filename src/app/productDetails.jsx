d8eea22ced58b56cce94c07ba1bd20627bd77b24
Christopher Deng Jr <cdengjr@smu.edu>
2021-11-04 01:50:15 -0500

    Update productDetails.jsx

--- src/app/productDetails.jsx
+++ src/app/productDetails.jsx
@@ -4,8 +4,8 @@ import { Product } from '../models/product';
 
 
 export class ProductDetails extends React.Component {
-   Product =[
-    new Product (
+   product =[
+     new Product(
         1,
         "Jif Peanut Butter, 40 ounce",
         "7g of Protein per serving with no preservatives, artifical flavors or colors.",
@@ -19,17 +19,39 @@ export class ProductDetails extends React.Component {
     render(){
         return(
             <>
-            <div style={{background:"#EEE", padding:"0.5rem 1rem", marginLeft:"1rem", marginRight:"1rem", marginBottom:"1rem"}}>
-                <nav aria-label="breadcrumb">
-                <ol class="breadcrumb">
-                <li class="breadcrumb-item"><a href="#">Tasty snacks</a></li>
-                <li class="breadcrumb-item active" aria-current="page">Jif Peanut Butter, 40 ounce</li>
-                </ol>
-                </nav>
+            <div style={{background:"#EEE", padding:"0.5rem 1rem", marginLeft:"3rem", marginRight:"3rem", marginBottom:"1rem"}}>
+                <NavBar product={this.product}/>
             </div>
-            <div>shit</div>
+            
+            <div style={{background:"#EEE", padding:"0.5rem 1rem", marginLeft:"3rem", marginRight:"3rem", marginBottom:"1rem"}}>
+               <ProductDesc product={this.product}/>
+            </div>
+            <div>something</div>
             </>
         )
     }
     
 }
+export const NavBar = (props) => (
+    <>
+    <nav aria-label="breadcrumb">
+        <ol class="breadcrumb">
+        <li class="breadcrumb-item"><a href="#">Tasty snacks</a></li>
+        <li class="breadcrumb-item active" aria-current="page">{ props.product.map(x => x.name)}</li>            </ol>
+    </nav>   
+                
+    </>
+  );
+
+
+export const ProductDesc = (props) => (
+    <>
+    <div class="jumbotron" >
+    <div><img src={props.product.map(x=>x.imageUrl)} style={{float:"left"}}/></div>
+    <h1 class="display-4" >{props.product.map(x=>x.name)}</h1>
+    <h3><span class="badge bg-primary" >${props.product.map(x=>x.price)}</span></h3>
+    <p class="lead">{props.product.map(x=>x.description)}.</p>
+    </div>
+    <div style={{clear:"both"}}/>
+    </>
+  );
